96a13f36600c5129b96041eae0169996ece594c6
garvitagr <114680476+garvitagr@users.noreply.github.com>
2023-10-09 15:19:29 +0530

    Time: 11 ms (50.4%), Space: 16.4 MB (27.87%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int,int>mp;
-        int i;
+        
+        // By using boyer's moore majority voting algorithm
+        
+        int ele1=-1,ele2=-1,ct1=0,ct2=0,ct=0,i,n=nums.size();
         vector<int>ans;
-        for(i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        
+        for(auto ele:nums){
+            if(ele==ele1)ct1++;
+            else if(ele==ele2)ct2++;
+            else if(ct1==0){
+                ele1=ele;
+                ct1++;
+            }
+            else if(ct2==0){
+                ele2=ele;
+                ct2++;
+            }
+            else{
+                ct1--;ct2--;
+            }
         }
-        for(auto it:mp){
-            if(it.second > nums.size()/3)ans.push_back(it.first);
+        ct1=0;ct2=0;
+        for(auto ele:nums){
+            if(ele==ele1)ct1++;
+            else if(ele==ele2)ct2++;
         }
+        if(ct1>n/3)ans.push_back(ele1);
+        if(ct2>n/3)ans.push_back(ele2);
+
         return ans;
     }
 };
