f0c26e2194122112df26a269bd9de371cf5dca1d
garvitagr <114680476+garvitagr@users.noreply.github.com>
2023-10-12 22:34:49 +0530

    Time: 23 ms (87.51%), Space: 27.8 MB (23.66%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int mid,s=0,e=nums.size()-1;
-        while(s<=e){
-            mid=s+(e-s)/2;
-            if(nums[mid]==target)return mid;
-            else if(nums[mid]>target)e=mid-1;
-            else s=mid+1;
+
+    int bs(vector<int>&nums,int target,int s,int e){
+        if(s>e)return -1;
+       int mid=s+(e-s)/2;
+        if(nums[mid]==target){
+            return mid;
         }
-        return -1;
+        else if(nums[mid]>target){
+            e=mid-1;
+            return bs(nums,target,s,e);
+        }
+        else {
+            s=mid+1;
+            return bs(nums,target,s,e);
+        }   
+    }
+
+    int search(vector<int>& nums, int target) {
+        return bs(nums,target,0,nums.size()-1);
     }
 };
