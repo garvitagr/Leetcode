a89fca545f8f57b85c092a453f42209120680bd2
garvitagr <114680476+garvitagr@users.noreply.github.com>
2023-10-21 22:40:14 +0530

    Time: 4 ms (74.66%), Space: 11.4 MB (46.89%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    void f(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&ds,int i){
+        if(i==candidates.size()){
+           if(target==0) ans.push_back(ds);
+            return;
+        }
+        
+        if(target-candidates[i]>=0){
+            ds.push_back(candidates[i]);
+            f(candidates,target-candidates[i],ans,ds,i);
+            ds.pop_back();
+        }
+        f(candidates,target,ans,ds,i+1);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        f(candidates,target,ans,ds,0);
+        return ans;
+    }
+};
