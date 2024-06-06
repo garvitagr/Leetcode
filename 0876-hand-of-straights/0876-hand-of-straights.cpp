db11e33779e5d939e9ba02d31952c44a144cfefe
garvitagr <114680476+garvitagr@users.noreply.github.com>
2024-06-06 23:06:00 +0530

    Time: 67 ms (35.03%), Space: 30.6 MB (69.25%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size()%groupSize!=0)return false;
+        map<int,int>mp;
+        int i;
+        for(int i=0;i<hand.size();i++)mp[hand[i]]++;
+
+        for(auto [hand,freq]:mp){
+            if(freq==0)continue;
+            while(freq--){
+                for(i=hand;i<hand+groupSize;i++){
+                    if(mp[i]==0)return false;
+                    mp[i]--;
+                }
+            }
+            
+        }
+        return true;
+    }
+};
