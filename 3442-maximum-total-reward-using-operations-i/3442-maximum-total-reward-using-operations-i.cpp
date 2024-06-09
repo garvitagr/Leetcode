d426b3249c3ec41010944114ee98ddbf41fde9d2
garvitagr <114680476+garvitagr@users.noreply.github.com>
2024-06-09 18:06:53 +0530

    Time: 690 ms (20%), Space: 431 MB (60%) - LeetHub

--- 3442-maximum-total-reward-using-operations-i/3442-maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/3442-maximum-total-reward-using-operations-i.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    // unordered_map<int,unordered_map<int,int>>dp;
     int helper(vector<int>&v,int ind,int cursum,vector<vector<int>>&dp){
         if(ind==v.size())return cursum;
         if(dp[ind][cursum]!=-1)return dp[ind][cursum];
@@ -15,9 +14,26 @@ public:
 
     int maxTotalReward(vector<int>& rewardValues) {
         sort(rewardValues.begin(),rewardValues.end());
-        // int sum=0,i;
-        // for(i=0;i<rewardValues.size();i++)sum+=rewardValues[i];
-        vector<vector<int>>dp(rewardValues.size()+10,vector<int>(10000+10,-1));
-        return helper(rewardValues,0,0,dp);
+        vector<vector<int>>dp(rewardValues.size()+10,vector<int>(10000+10,0));
+        // return helper(rewardValues,0,0,dp);
+
+        int i,j;
+        
+        for(i=rewardValues.size();i>=0;i--){
+            for(j=5000;j>=0;j--){
+                if(i==rewardValues.size())dp[i][j]=j;
+                else{
+                    int take=0,nottake=0;
+                    if(rewardValues[i]>j){
+                        take=dp[i+1][j+rewardValues[i]];
+                    }
+                    nottake=dp[i+1][j];
+
+                    dp[i][j]=max(take,nottake);
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
