f8d3045d280708152be6dfab7458809aaf9516b8
garvitagr <114680476+garvitagr@users.noreply.github.com>
2024-07-15 14:48:17 +0530

    Time: 2793 ms (5%), Space: 506.4 MB (5.01%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
+        set<vector<int>>tempans;
+        int i,j;
         sort(nums.begin(),nums.end());
-        unordered_map<int,int>mp;
-        vector<vector<int>>ans,result;
-        for(int i=0;i<nums.size();i++)mp[nums[i]]=i;
-
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                int thirdval=-(nums[i]+nums[j]);
-                if(mp.find(thirdval)!=mp.end() and mp[thirdval]>j)ans.push_back({nums[i],nums[j],thirdval});
+        for(i=0;i<nums.size();i++){
+            for(j=i+1;j<nums.size();j++){
+                int ele=nums[i]+nums[j];
+                ele*=-1;
+                int ind=lower_bound(nums.begin()+j+1,nums.end(),ele)-nums.begin();
+                if(ind!=nums.size() and nums[ind]==ele){
+                    vector<int>temp={nums[i],nums[j],nums[ind]};
+                    tempans.insert(temp);
+                }
             }
         }
-        sort(ans.begin(),ans.end());
-        for(auto it:ans){
-            if(result.size() and result.back()==it)continue;
-            result.push_back(it);
+        for(auto it:tempans){
+            ans.push_back(it);
         }
-        return result;
+        return ans;
     }
 };
