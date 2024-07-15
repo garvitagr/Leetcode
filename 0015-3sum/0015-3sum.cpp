20c452045e5e53617c7b9505169e147f8328d5fe
garvitagr <114680476+garvitagr@users.noreply.github.com>
2024-07-15 14:50:58 +0530

    Time: 2842 ms (5%), Space: 504.6 MB (5.01%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
-        set<vector<int>>tempans;
-        int i,j;
         sort(nums.begin(),nums.end());
+        vector<vector<int>>result;
+        set<vector<int>>ans;
+        int i,j,ind,thirdval;
         for(i=0;i<nums.size();i++){
             for(j=i+1;j<nums.size();j++){
-                int ele=nums[i]+nums[j];
-                ele*=-1;
-                int ind=lower_bound(nums.begin()+j+1,nums.end(),ele)-nums.begin();
-                if(ind!=nums.size() and nums[ind]==ele){
-                    vector<int>temp={nums[i],nums[j],nums[ind]};
-                    tempans.insert(temp);
-                }
+                thirdval=-(nums[i]+nums[j]);
+                ind=lower_bound(nums.begin()+j+1,nums.end(),thirdval)-nums.begin();
+                if(ind!=nums.size() and nums[ind]==thirdval)ans.insert({nums[i],nums[j],thirdval});
             }
         }
-        for(auto it:tempans){
-            ans.push_back(it);
+        for(auto it:ans){
+            result.push_back(it);
         }
-        return ans;
+        return result;
     }
 };
