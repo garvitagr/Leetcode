30cdff1a6fa2e347e3a55b62dcd9d1beb57a5990
garvitagr <114680476+garvitagr@users.noreply.github.com>
2024-07-29 04:14:22 +0530

    Time: 540 ms (37.89%), Space: 211.8 MB (27.8%) - LeetHub

---
+++ 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    int find(int curtime,int time,int change){
+        int times=curtime/change;
+        if(times&1){
+            int rem=change*(times+1)-curtime;
+            return rem+curtime+time;
+        }
+        return curtime+time;
+    }
+
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+        vector<vector<int>>adj(n+1);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int>dist1(n+1,INT_MAX),dist2(n+1,INT_MAX);
+        set<pair<int,int>>st;
+        st.insert({0,1});     
+        dist1[1]=0;
+        vector<int>visited(n+1,0);
+
+        while(!st.empty()){
+            auto pr=*st.begin();
+            st.erase(pr);
+            int node=pr.second;
+            int curtime=pr.first;
+            // shortestdist[node]=min(shortestdist[node],curtime);
+            int nexttime=find(curtime,time,change);
+            visited[node]++;
+            // if(node==n){
+            //     if(curtime<minimum.first){
+            //         minimum.second=minimum.first;
+            //         minimum.first=curtime;
+            //     }
+            //     else if(curtime<minimum.second)minimum.second=curtime;
+            // }
+            if(visited[node]==2 and node==n)return dist2[n];
+
+            for(auto nbr:adj[node]){
+                if(visited[nbr]==2)continue;
+                if(dist1[nbr]>nexttime){
+                    dist2[nbr]=dist1[nbr];
+                    dist1[nbr]=nexttime;
+                    st.insert({nexttime,nbr});
+                }
+                else if(dist2[nbr]>nexttime and nexttime!=dist1[nbr]){
+                    dist2[nbr]=nexttime;
+                    st.insert({nexttime,nbr});
+                }
+            }
+
+        }
+
+        // while(!st.empty()){
+        
+        //     
+        //     shortestdist[node]=min(shortestdist[node],curtime);
+        //     
+        //     
+        //     if(visited[node]==2 and n==node)break;
+        //     if(visited[node]==2)continue;
+            
+        //     for(auto nbr:adj[node]){
+        //         if(nexttime<shortestdist[nbr])st.insert({nexttime,nbr});
+        //     }
+        // }
+        return dist2[n];
+    }   
+};
