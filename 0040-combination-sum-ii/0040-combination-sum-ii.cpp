548685aee5be94c9a0383adae126a5cd79eaa389
garvitagr <114680476+garvitagr@users.noreply.github.com>
2024-08-13 22:17:19 +0530

    Time: 3 ms (77.81%), Space: 13 MB (38.57%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
 
-    void comb(vector<vector<int>>&ans,vector<int>&candidates,vector<int>&ds,int target,int ind){
-        if(ind>=candidates.size()){
-           if(target==0) ans.push_back(ds);
+    void combination(int ind,int target,vector<int>&candidates,vector<int>&ds,vector<vector<int>>&ans){
+
+        if(target==0){
+            ans.push_back(ds);
             return;
         }
+
         int i;
         for(i=ind;i<candidates.size();i++){
-             // take number :
-        if(target-candidates[i]>=0){
+
+            if(i>ind and candidates[i]==candidates[i-1])continue;
+            if(candidates[i]>target)break;
             ds.push_back(candidates[i]);
-            comb(ans,candidates,ds,target-candidates[i],i+1);
+            combination(i+1,target-candidates[i],candidates,ds,ans);
             ds.pop_back();
-        }  
+        }
 
-        // leave number :
-
-        if(i>ind and candidates[i]==candidates[i-1]){
-            // no take and call for i+1
-            comb(ans,candidates,ds,target,i+1);
-        }  
     }
-}
+
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
-        vector<vector<int>>ans;
         vector<int>ds;
-        comb(ans,candidates,ds,target,0);
+        vector<vector<int>>ans;
+        combination(0,target,candidates,ds,ans);
         return ans;
     }
 };
