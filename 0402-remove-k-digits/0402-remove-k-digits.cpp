2d8a81318ba9a33955a334f837d312418639aa97
garvitagr <114680476+garvitagr@users.noreply.github.com>
2024-08-19 01:54:23 +0530

    Time: 11 ms (61.76%), Space: 16.5 MB (5.86%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,21 +1,36 @@
+#define ll long long
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-        stack<int>st;
-        st.push(0);
+    string removeKdigits(string s, int k) {
+        ll i,curpos=0,n=s.size(),dig;
         string ans;
-        for(int i=0;i<num.size();i++){
-            int val=num[i]-'0';
-            while((!st.empty()) and (st.top()>val) and (k>0))st.pop(),k--;
-            st.push(val);
+        vector<vector<ll>>pos(10);
+
+        for(i=0;i<n;i++){
+            pos[s[i]-'0'].emplace_back(i);
         }
-        while(!st.empty()){
-            ans+=st.top()+'0';
-            st.pop();
+        while(k>0){
+            ll remain=n-curpos;
+            if(remain==k){
+                curpos=n;break;
+            }
+            for(dig=0;dig<=9;dig++){
+                if(pos[dig].empty())continue;
+                ll ind=lower_bound(pos[dig].begin(),pos[dig].end(),curpos)-pos[dig].begin();
+                if(ind!=pos[dig].size())ind=pos[dig][ind];
+                else continue;
+                if(ind<=curpos+k){
+                    k-=ind-curpos;
+                    curpos=ind+1;
+                    ans+=(dig+'0');
+                    break;
+                }
+            }
         }
+        while(curpos<n)ans+=s[curpos++];
+        reverse(ans.begin(),ans.end());
         while(!ans.empty() and ans.back()=='0')ans.pop_back();
         reverse(ans.begin(),ans.end());
-        while(!ans.empty() and k>0)ans.pop_back(),k--;
         return (ans.empty()?"0":ans);
     }
 };
