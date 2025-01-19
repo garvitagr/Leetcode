29df53f813646d4b4d1621e47f529781d5ff7db2
garvitagr <114680476+garvitagr@users.noreply.github.com>
2025-01-19 23:19:21 +0530

    Time: 109 ms (10.66%), Space: 12.1 MB (84.52%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> minOperations(string boxes) {
+    vector<int>minOperations(string boxes) {
         vector<int>ans(boxes.size());
         for(int i=0;i<boxes.size();i++){
             int val=0;
